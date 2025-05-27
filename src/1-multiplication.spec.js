02a858e5856e1046e7c983c717a7f35e9aaf99ed
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-05-27 13:42:55 +0200

    git commit -m "Code-lab-web"

--- src/1-multiplication.spec.js
+++ src/1-multiplication.spec.js
@@ -3,5 +3,8 @@ import { multiply } from './1-multiplication'
 describe('1-multiplication', () => {
   it('should return the value from multiplying two numbers', () => {
     expect(multiply(2, 2)).toEqual(4)
+    export const multiply = (a, b) => {
+      return a * b
+    }
   })
 })
