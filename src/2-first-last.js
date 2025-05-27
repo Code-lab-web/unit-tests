4ee11caec7aba7f64a14c09b69fdcffb6467c2c5
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-05-27 15:16:24 +0200

    Add tests for firstLast function and fix return values

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,4 +1,17 @@
+import { first,Last} from './1- first-last.js';
+
+describe('2-first-last', () => {
+  it('should return the first items from an array with two items', () => {
+    expect(firstLast(['a', 'b'])).toEqual('First: a, Last: b')
+  })
+it('Should only return the first item if the array has one item', () => {
+  expect(firstLast(['bob'])).toEqual('Only item:bob')
+})
+it('Should return "No items!" if the array is empty', () => {
+  expect(firstLast([])).toEqual('No items!')
+})
+});
 export const firstLast = (items) => {
-  return `First: ${items[1]}, Last: ${items[1]}`
+  return `First: ${items[0]}, Last: ${items[items.length - 1]}`
 
 }
